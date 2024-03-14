2033f022eda31abdf523a6d65972302ef6d167e5
Chamu Mutezva <ckmutezva@gmail.com>
2024-03-14 04:51:23 +0200

    commentSchema update

--- blog-reactQuery/bloglist-backend/controllers/comment.js
+++ blog-reactQuery/bloglist-backend/controllers/comment.js
@@ -1,4 +1,5 @@
 const commentRouter = require("express").Router();
+const Blog = require("../models/blog");
 const Comment = require("../models/comment"); // Import your Comment model
 
 // Create a new comment for a blog
@@ -6,11 +7,13 @@ commentRouter.post("/api/blogs/:id/comments", async (req, res) => {
     try {
         const { content } = req.body;
         const blogId = req.params.id; // Get the blog ID from the URL
-
+        const blog = await Blog.findById(blogId);
+        console.log(blog);
         // Create a new comment
         const comment = new Comment({ content, blog: blogId });
         await comment.save();
-
+     //   blog.comment = blog.comment.concat(blogId);
+        
         res.status(201).json(comment);
     } catch (error) {
         res.status(500).json({ error: "Server error" });
@@ -18,8 +21,10 @@ commentRouter.post("/api/blogs/:id/comments", async (req, res) => {
 });
 
 commentRouter.get("/api/blogs/:id/comments", async (request, response) => {
+    console.log(request.params.id);
     const comments = await Comment.find({ blogs: request.params.id });
+    console.log(comments);
     response.json(comments);
-  });
+});
 
 module.exports = commentRouter;
--- blog-reactQuery/bloglist-backend/models/comment.js
+++ blog-reactQuery/bloglist-backend/models/comment.js
@@ -7,4 +7,12 @@ const commentSchema = new mongoose.Schema({
 
 const Comment = mongoose.model("Comment", commentSchema);
 
+commentSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+
 module.exports = Comment;
